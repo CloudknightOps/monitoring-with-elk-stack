00bf62313c148afd84187588d32f45879f19ac0f
Theo <uviekugbere.theo@gmail.com>
2025-08-30 10:27:35 +0100

    Adding k8s manifests

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -9,56 +9,94 @@ resource "aws_security_group" "sg_main" {
   }
 }
 
-# Allow SSH access (port 22)
+# -------------------------------
+# Publicly accessible ports
+# -------------------------------
+
+# Allow SSH (recommend restrict to your IP instead of 0.0.0.0/0)
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   security_group_id = aws_security_group.sg_main.id
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0" # <-- change to your IP
   from_port         = 22
-  ip_protocol       = "tcp"
   to_port           = 22
-
-  tags = {
-    Name = "allow_ssh_ipv4"
-  }
+  ip_protocol       = "tcp"
 }
 
-# Allow kube-api (6443)
+# Allow kube-api (workers + kubectl)
 resource "aws_vpc_security_group_ingress_rule" "allow_6443_ipv4" {
   security_group_id = aws_security_group.sg_main.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 6443
-  ip_protocol       = "tcp"
   to_port           = 6443
-
-  tags = {
-    Name = "allow_6443_ipv4"
-  }
+  ip_protocol       = "tcp"
 }
 
-# Allow etcd (2379)
-resource "aws_vpc_security_group_ingress_rule" "allow_2379_ipv4" {
-  security_group_id = aws_security_group.sg_main.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 2379
-  ip_protocol       = "tcp"
-  to_port           = 2379
+# -------------------------------
+# Internal cluster-only ports
+# -------------------------------
+# Restrict to same SG (node-to-node)
 
-  tags = {
-    Name = "allow_2379_ipv4"
-  }
+# etcd (2379–2380)
+resource "aws_vpc_security_group_ingress_rule" "allow_etcd_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 2379
+  to_port                      = 2380
+  ip_protocol                  = "tcp"
 }
 
-# Allow etcd (2380)
-resource "aws_vpc_security_group_ingress_rule" "allow_2380_ipv4" {
-  security_group_id = aws_security_group.sg_main.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 2380
-  ip_protocol       = "tcp"
-  to_port           = 2380
+# RKE2 Supervisor (9345)
+resource "aws_vpc_security_group_ingress_rule" "allow_9345_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 9345
+  to_port                      = 9345
+  ip_protocol                  = "tcp"
+}
 
-  tags = {
-    Name = "allow_2380_ipv4"
-  }
+# Kubelet / Controller Manager / Scheduler (10250–10255)
+resource "aws_vpc_security_group_ingress_rule" "allow_kubelet_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 10250
+  to_port                      = 10255
+  ip_protocol                  = "tcp"
+}
+
+# Kube-Proxy (10256)
+resource "aws_vpc_security_group_ingress_rule" "allow_kubeproxy_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 10256
+  to_port                      = 10256
+  ip_protocol                  = "tcp"
+}
+
+# Flannel VXLAN (UDP 8472)
+resource "aws_vpc_security_group_ingress_rule" "allow_flannel_vxlan_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 8472
+  to_port                      = 8472
+  ip_protocol                  = "udp"
+}
+
+# Calico VXLAN (UDP 4789)
+resource "aws_vpc_security_group_ingress_rule" "allow_calico_vxlan_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 4789
+  to_port                      = 4789
+  ip_protocol                  = "udp"
+}
+
+# Calico Typha / BGP (TCP 5473)
+resource "aws_vpc_security_group_ingress_rule" "allow_calico_tcp_internal" {
+  security_group_id            = aws_security_group.sg_main.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 5473
+  to_port                      = 5473
+  ip_protocol                  = "tcp"
 }
 
 # Allow all outbound traffic
@@ -66,10 +104,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.sg_main.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
-
-  tags = {
-    Name = "allow_all_traffic_ipv4"
-  }
 }
 
 # ------------------------------------------------------------------
@@ -111,12 +145,14 @@ resource "aws_vpc_security_group_ingress_rule" "elk_allow_5601_ipv4" {
   to_port           = 5601
 }
 
+
+# Allow Filebeat (workers) -> Logstash (5044)
 resource "aws_vpc_security_group_ingress_rule" "elk_allow_5044_workers" {
-  security_group_id = aws_security_group.sg_elk.id
-  cidr_ipv4         =  "0.0.0.0/0"
-  from_port         = 5044
-  ip_protocol       = "tcp"
-  to_port           = 5044
+  security_group_id            = aws_security_group.sg_elk.id
+  referenced_security_group_id = aws_security_group.sg_main.id
+  from_port                    = 5044
+  to_port                      = 5044
+  ip_protocol                  = "tcp"
 }
 
 # Allow all outbound traffic
